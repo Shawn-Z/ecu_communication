19435352df432b26964a9ac9b03668acb4b7c37e
shawn-z <2120170431@bit.edu.cn>
2018-12-03 23:49:41 +0800

    new time check

--- src/ECUCommunication.cpp
+++ src/ECUCommunication.cpp
@@ -1,30 +1,7 @@
 #include "CommunicationProcess.hpp"
 
-bool process(char *buffer, size_t buffer_size) {
-    ROS_INFO_STREAM(sizeof(buffer));
-    ROS_INFO_STREAM((int)buffer[0]);
-    ROS_INFO_STREAM((int)buffer[1]);
-    ROS_INFO_STREAM((int)buffer[2]);
-    ROS_INFO_STREAM((int)buffer[3]);
-    return true;
-}
-
-uint8_t * tst() {
-    uint8_t data[3] = {1,2,3};
-    return data;
-}
-
 int main(int argc, char** argv) {
     ros::init(argc, argv, "ecu_communication");
     ecu_communication::CommunicationProcess communicationProcess(ros::NodeHandle(), ros::NodeHandle("~"));
-    uint8_t data[9] = {1,2,3};
-    process((char *)data, 1);
-
-
-
-    communicationProcess.udpReceive();
-
-
-
     ros::spin();
 }
